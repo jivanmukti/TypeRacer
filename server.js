ab2d1243b49bf8b37f9a15aaf437c712fcc5e729
Michael Dalton <modalton@usc.edu>
2018-02-14 11:29:05 -0800

    restored server

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 const app = express();
 const http = require('http').Server(app);
-var io = require('socket.io')(http, { origins: '*'});
+var io = require('socket.io')(http, { origins: 'https://immense-tor-65130.herokuapp.com/'});
 const raceHandler = require('./socketHandlers/raceHandler.js');
 
 let room = 0;
